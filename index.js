74af41be8c1c3c4fc71eba3a34c2481f498fc9f0
Rosa <rcruz12@ucol.mx>
2023-10-25 22:31:59 -0600

    Second version

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // Codigo que el pida al usuario su edad y si es mayor de edad le pregunte su nombre y apellido y lo salude en el DOM
 // Si no, le ponga en el DOM lo siento, no eres mayor de edad
 
-let edad = prompt("¿Cual es tu edad?");
-if (edad >= 18) {
-    let nombre = prompt("Escribe un nombre y un apellido")
-    document.write ("Hola"+" "+nombre);
+// let edad = prompt("¿Cual es tu edad?");
+// if (edad >= 18) {
+//     let nombre = prompt("Escribe un nombre y un apellido")
+//     document.write ("Hola"+" "+nombre);
+// } else {
+//     document.write ("Lo siento, eres menor de edad");
+// }
+
+function tarea (){
+
+let cali1= parseFloat(document.getElementById("cal1").value);
+let cali2= parseFloat (document.getElementById("cal2").value);
+let cali3= parseFloat(document.getElementById("cal3").value);
+res1 = cali1 + cali2 + cali3
+promedio = res1/3
+if (promedio>=8){
+    document.write ("Felicidades lo lograste")
 } else {
-    document.write ("Lo siento, eres menor de edad");
+    document.write ("¡Sigue estudiando!")
+}
 }
